cea6adb2ddf0a9a1ed3c1e329303d1a0eddaec52
Mike McQueen <mmcqueen@ruralking.com>
2019-12-09 10:46:23 -0600

    Revert "eslint tab etc issues, revert dockerfile and nginx config, added title"
    This reverts commit 84034eba7649f0a0925f672ea18aeaed641c9927.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# skywalker-countdown
+# vuetube
 
 > jedi-countdown
 
--- src/components/timer.vue
+++ src/components/timer.vue
@@ -1,124 +1,131 @@
 <template>
-  <div class="wrapper">
-    <div class="time">
-      {{ days | beautify }}
-      <div class="time__mode">
-        Days
-      </div>
+    <div class="wrapper">
+        <div class="time">
+            {{ days | beautify }}
+            <div class="time__mode">
+                Days
+            </div>
+        </div>
+        <div class="time">
+            {{ hours | beautify }}
+            <div class="time__mode">
+                Hours
+            </div>
+
+        </div>
+        <div class="time">
+            {{ minutes| beautify }}
+            <div class="time__mode">
+                Minutes
+            </div>
+
+        </div>
+        <div class="time">
+            {{ seconds | beautify }}
+            <div class="time__mode">
+                Seconds
+            </div>
+
+        </div>
+
     </div>
-    <div class="time">
-      {{ hours | beautify }}
-      <div class="time__mode">
-        Hours
-      </div>
-    </div>
-    <div class="time">
-      {{ minutes | beautify }}
-      <div class="time__mode">
-        Minutes
-      </div>
-    </div>
-    <div class="time">
-      {{ seconds | beautify }}
-      <div class="time__mode">
-        Seconds
-      </div>
-    </div>
-  </div>
 </template>
 <style>
-  .wrapper {
-    width: 100%;
-    position: absolute;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    -webkit-transform: translate(-50%, -50%);
-    -moz-transform: translate(-50%, -50%);
-    -o-transform: translate(-50%, -50%);
-    top: 10%;
-    left: 50%;
-    dislpay: inline-block;
-    text-align: center;
-    font-family: "Lato", Arial, sans-serif;
-    font-weight: normal;
-    color: #d70000;
-  }
+    .wrapper {
+        width: 100%;
+        position: absolute;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+        -moz-transform: translate(-50%, -50%);
+        -o-transform: translate(-50%, -50%);
+        top: 10%;
+        left: 50%;
+        dislpay: inline-block;
+        text-align: center;
+        font-family: "Lato", Arial, sans-serif;
+        font-weight: normal;
+        color: #d70000;
+    }
 
-  .time {
-    display: inline-block;
-    padding: 20px;
-    font-size: 7em;
-    font-weight: 700;
-    text-shadow: 0px 5px 20px rgba(0,0,0,0.7)
-  }
-
-  @media screen and (max-width: 768px) {
     .time {
-      font-size: 2em;
+        display: inline-block;
+        padding: 20px;
+        font-size: 7em;
+        font-weight: 700;
+        text-shadow: 0px 5px 20px rgba(0,0,0,0.7)
     }
-  }
 
-  .time .time__mode {
-    font-size: 25px;
-    font-weight: 300;
-    margin: 7px 0;
-    letter-spacing: 4px;
-    text-shadow: 0px 4px 14px rgba(0,0,0,0.5)
-  }
+    @media screen and (max-width: 768px) {
+        .time {
+            font-size: 2em;
+        }
+    }
 
-  @media screen and (max-width: 768px) {
     .time .time__mode {
-      font-size: 12px;
-      letter-spacing: normal;
+        font-size: 25px;
+        font-weight: 300;
+        margin: 7px 0;
+        letter-spacing: 4px;
+        text-shadow: 0px 4px 14px rgba(0,0,0,0.5)
     }
-  }
+
+    @media screen and (max-width: 768px) {
+        .time .time__mode {
+            font-size: 12px;
+            letter-spacing: normal;
+        }
+    }
 </style>
 <script>
-export default{
-  props: ['dateProps'],
-  mounted: function () {
-    this.$nextTick(function () {
-      window.setInterval(() => {
-        this.current = Math.trunc((new Date()).getTime() / 1000)
-      }, 1000)
-    })
-  },
+    export default{
+        props: ['dateProps'],
+        mounted: function () {
+            this.$nextTick(function () {
+                window.setInterval(() => {
+                    this.current = Math.trunc((new Date()).getTime() / 1000);
+            },
+                1000
+                )
+                ;
+            });
+        },
 
-  data () {
-    return {
-      current: Math.trunc((new Date()).getTime() / 1000),
-      date: Math.trunc(Date.parse(this.dateProps) / 1000)
+        data() {
+            return {
+                current: Math.trunc((new Date()).getTime() / 1000),
+                date: Math.trunc(Date.parse(this.dateProps) / 1000)
+            }
+        },
+
+        computed: {
+            days() {
+                return Math.trunc((this.date - this.current) / 3600 / 24);
+            },
+
+            hours() {
+                return Math.trunc((this.date - this.current) / 3600) % 24;
+            },
+
+            minutes() {
+                return Math.trunc((this.date - this.current) / 60) % 60;
+            },
+
+            seconds() {
+                return Math.trunc((this.date - this.current)) % 60;
+            }
+        },
+
+        filters: {
+            beautify: function (value) {
+                let temp = value.toString();
+
+                if (temp.length <= 1) {
+                    return "0" + value.toString();
+                } else {
+                    return value.toString();
+                }
+            }
+        }
     }
-  },
-
-  computed: {
-    days () {
-      return Math.trunc((this.date - this.current) / 3600 / 24)
-    },
-
-    hours () {
-      return Math.trunc((this.date - this.current) / 3600) % 24
-    },
-
-    minutes () {
-      return Math.trunc((this.date - this.current) / 60) % 60
-    },
-
-    seconds () {
-      return Math.trunc((this.date - this.current)) % 60
-    }
-  },
-
-  filters: {
-    beautify: function (value) {
-      let temp = value.toString()
-
-      if (temp.length <= 1) {
-        return '0' + value.toString()
-      } else {
-        return value.toString()
-      }
-    }
-  }
-}
 </script>
